97978308ff42b036dcf982c532d035586aca05d1
Alec Hyde <alechyde91@gmail.com>
2018-08-29 16:51:30 -0700

    tests for store are passing

--- src/store/store.js
+++ src/store/store.js
@@ -2,4 +2,20 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './../reducers/main.js';
 
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+// const store = createStore(rootReducer, ['Use Redux'])
+
+// export default store;
+
+// const store = new Store(reducers, initialState);
+
+// const configureStore = preloadedState => createStore(
+//   rootReducer,
+//   preloadedState,
+//   applyMiddleware(thunk, api)
+// )
+
+export default store;
+
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
